6d2e3adddb13cb841efb4e2a58092420d5bc53ff
danajacobc <danajacobc@gmail.com>
2024-05-16 10:02:42 -0300

    securityFilterChain

--- src/main/java/com/pdm/pdmonitoreo/PdMonitoreoApplication.java
+++ src/main/java/com/pdm/pdmonitoreo/PdMonitoreoApplication.java
@@ -4,26 +4,29 @@ import com.pdm.pdmonitoreo.domain.repositories.PaisRepository;
 import com.pdm.pdmonitoreo.domain.repositories.PlantasRepository;
 import com.pdm.pdmonitoreo.domain.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@Slf4j
-public class PdMonitoreoApplication {
+public class PdMonitoreoApplication implements CommandLineRunner {
 
-	private final UserRepository userRepository;
-	private final PlantasRepository plantasRepository;
-	private final PaisRepository paisRepository;
-
-    public PdMonitoreoApplication(UserRepository userRepository, PlantasRepository plantasRepository, PaisRepository paisRepository) {
-        this.userRepository = userRepository;
-        this.plantasRepository = plantasRepository;
-        this.paisRepository = paisRepository;
-    }
-
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
 		SpringApplication.run(PdMonitoreoApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        this.userRepository.findAll()
+                .forEach(user -> System.out.println(user.getEmail() + "-" + this.bCryptPasswordEncoder.encode(user.getPassword())));
+    }
 
 }
--- src/main/java/com/pdm/pdmonitoreo/config/SecurityConfig.java
+++ src/main/java/com/pdm/pdmonitoreo/config/SecurityConfig.java
@@ -2,10 +2,28 @@ package com.pdm.pdmonitoreo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfig {
+
+    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+
+        http.exceptionHandling(e -> e.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint(LOGIN_RESOURCE)));
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
