f545ce64f2c605c7c0f55faebf219e5bc1069f3c
danajacobc <danajacobc@gmail.com>
2024-05-16 12:23:41 -0300

    login authcontroller

--- src/main/java/com/pdm/pdmonitoreo/Auth/AuthController.java
+++ src/main/java/com/pdm/pdmonitoreo/Auth/AuthController.java
@@ -1,7 +1,9 @@
 package com.pdm.pdmonitoreo.Auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @PostMapping(value = "login")
-    public String login()
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
     {
-        return "login from public endpoint";
+        return ResponseEntity.ok(new AuthResponse());
     }
 
 //    @PostMapping(value = "register")
